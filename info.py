16c45ca228a845aadbe8b7c6cb735c1a818f3527
AF <gulabsinghrajput008@gmail.com>
2025-02-16 20:58:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,8 +11,8 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_ID = int(environ.get('API_ID', '21642624'))
+API_HASH = environ.get('API_HASH', '11af98e6c638725a6068667d3fc39c05')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
