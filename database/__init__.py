52b8f60cd1725b24feac7c432bb4b8f942f0e640
AF <gulabsinghrajput008@gmail.com>
2025-02-28 16:57:22 +0530

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,11 +1,11 @@
 import os
 from pymongo import MongoClient
 
-# MongoDB कनेक्शन
+# ✅ MongoDB कनेक्शन सेट करें
 DATABASE_URI = os.getenv("DATABASE_URI", "")
 DATABASE_NAME = "techvjclonefilterbot"
 COLLECTION_NAME = "vjcollection"
 
 client = MongoClient(DATABASE_URI)
 db = client[DATABASE_NAME]
-collection = db[COLLECTION_NAME]  # ✅ अब collection यहां पर सेट हो गया है
+collection = db[COLLECTION_NAME]  # ✅ अब सही से MongoDB कनेक्ट होगा
